945f30301e6e03055bbafde47e80796fad2d0f96
Supab Saelim <supab.saelim@empyrean-apac.com>
2019-02-26 08:13:07 +0700

    remove unused file

--- dbreset.ps1
+++ dbreset.ps1
@@ -3,7 +3,7 @@ cd "C:\Users\SupabSaelim\ifrs9\databases"
 python C:\Users\SupabSaelim\Desktop\ueli_utils\replaceManifest.py
 python dbrestore.py -r drop-all
 python dbinstall.py
-python dbrestore.py -r demo-data
+python dbrestore.py -f -r demo-data
 python dblicense.py -f dev-0303
 # ((Get-Content -path ./localDbConfig.json -Raw) -replace '1.0.0','latest') | Set-Content -Path ./localDbConfig.json
 python C:\Users\SupabSaelim\Desktop\ueli_utils\replaceLatest.py
--- dbreset_v1.ps1
+++
@@ -1,9 +0,0 @@
-cd "C:\Users\SupabSaelim\ifrs9\databases"
-((Get-Content -path ./localDbConfig.json -Raw) -replace 'latest','1.0.0') | Set-Content -Path ./localDbConfig.json
-python dbrestore.py -r drop-all
-python dbinstall.py
-python dbrestore.py -r demo-data-1.0
-python dblicense.py -f dev-0303
-((Get-Content -path ./localDbConfig.json -Raw) -replace '1.0.0','latest') | Set-Content -Path ./localDbConfig.json
-python dbinstall.py
-cmd
