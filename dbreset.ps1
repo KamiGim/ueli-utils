3ab8d7d09733d8b95e12a932042f032d1436bead
Supab Saelim <supab.saelim@empyrean-apac.com>
2019-03-04 00:31:47 +0700

    update license

--- dbreset.ps1
+++ dbreset.ps1
@@ -4,7 +4,7 @@ python C:\Users\SupabSaelim\Desktop\ueli_utils\replaceManifest.py
 python dbrestore.py -r drop-all
 python dbinstall.py
 python dbrestore.py -f -r demo-data
-python dblicense.py -f dev-0303
+python dblicense.py -f dev-0304
 # ((Get-Content -path ./localDbConfig.json -Raw) -replace '1.0.0','latest') | Set-Content -Path ./localDbConfig.json
 python C:\Users\SupabSaelim\Desktop\ueli_utils\replaceLatest.py
 python dbinstall.py
